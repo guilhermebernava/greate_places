ddb70185c419bf34d413a16187a9b1ebab8f3fbf
Gustavo Lira Criado <ext.gustavo.lira@motz.com.br>
2022-08-12 19:36:07 -0300

    remove warnings

--- lib/core/themes/app_text_style.dart
+++ lib/core/themes/app_text_style.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
-import 'package:greate_places/core/themes/app_colors.dart';
 
 class AppTextStyle {
   static final appBarTitle = GoogleFonts.manrope(
